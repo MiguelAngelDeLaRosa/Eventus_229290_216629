20daaa33e9e11d97a96eb3b46c77de328660a923
MiguelAngelDeLaRosa <miguel.delarosa229290@potros.itson.edu.mx>
2024-04-05 14:41:32 -0600

    Layout select_evento
    Desarrollo de la pantalla para seleccionar evento

---
+++ app/src/main/java/mx/edu/itson/potros/eventus/SelectTipoEvento.kt
@@ -0,0 +1,11 @@
+package mx.edu.itson.potros.eventus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectTipoEvento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_tipo_evento)
+    }
+}
